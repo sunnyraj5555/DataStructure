22974ee41922b35275c60edf6813d6b99058ff02
sunnyraj5555 <sr55555sun@gmail.com>
2022-01-21 11:18:18 +0530

    Created Linked List

---
+++ insertion_in_linkedlist.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+  public:
+    int data;
+    node* link;
+    
+    // constructor
+    node(int val){
+        data = val;
+        link = NULL;
+    }
+};
+
+
